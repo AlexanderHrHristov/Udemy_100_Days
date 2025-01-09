12bacbe8d00f7426aaf9389817d7354204cb30ff
Alexander Hristov <alexander.hristov@stingpharma.com>
2025-01-09 11:38:42 +0200

    Updated BMI Calculator with new logic

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -3,22 +3,22 @@ weight = float(input("Enter your WEIGHT in kilograms:\n"))
 
 bmi = weight / ((height/100) ** 2)
 
-print(f"Your BMI is {bmi:.2f}")
+print(f"Your BMI is {bmi:.1f}")
 
 if bmi < 18.5:
-    print("Your Weight is UNDERWEIGHT")
+    print("Your weight is considered UNDERWEIGHT")
 
-elif 18.5 >= bmi <= 25:
-    print("Your Weight is NORMAL")
+elif 18.5 <= bmi <= 25:
+    print("Your weight is considered NORMAL")
 
-elif 25 > bmi <= 30:
-    print("Your Weight is OVERWEIGHT")
+elif 25 < bmi <= 30:
+    print("Your weight is considered OVERWEIGHT")
 
-elif 30 > bmi <= 35:
-    print("Your are OBESE Class I")
+elif 30 < bmi <= 35:
+    print("You are classified as OBESE Class I")
 
-elif 35 > bmi <= 40:
-    print("Your are OBESE Class II")
+elif 35 < bmi <= 40:
+    print("You are classified as OBESE Class II")
 
 elif bmi > 40:
-    print("Your are OBESE Class III")
+    print("You are classified as OBESE Class III")
